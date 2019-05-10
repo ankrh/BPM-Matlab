ef07ad85edf43f298f4c953d7384caebc781540b
Anders Kragh Hansen <ankrh@fotonik.dtu.dk>
2019-05-10 16:56:02 +0200

    WIP, realized that the formula in FDBPMPaper.pdf was indeed correct but most literature does not mention that the formula is only valid if n is approximately equal to n_0.

--- FD_BPM.m
+++ FD_BPM.m
@@ -5,31 +5,35 @@
 % in 2D
 % Reference: An assessment of FD BPM method - Youngchul Chung
 % ***************************************************************************************************
+format long
+format compact
 
 %% User-specified general parameters
-Lx_main = 200e-6;                                                     % [m] x side length of main area
-Ly_main = 20e-6;                                                    % [m] y side length of main area
-Lz = 10e-3;                                                          % [m] z propagation distance
-targetzstepsize = 100e-6;                                          % [m] z step size to aim for
+Lx_main = 40e-6;                                                     % [m] x side length of main area
+Ly_main = 40e-6;                                                    % [m] y side length of main area
+Lz = 0.003;                                                         % [m] z propagation distance
+targetzstepsize = 1e-6;                                          % [m] z step size to aim for
 
-Nx_main = 51;                                                        % x resolution of main area
-Ny_main = 11;                                                             % y resolution of main area
+Nx_main = 60;                                                        % x resolution of main area
+Ny_main = 60;                                                             % y resolution of main area
 
 lambda = 1e-6;                                                % [m] Wavelength 
-w = 20e-6;%3.9e-6;                                                       % [m] Initial waist plane 1/e^2 radius of the gaussian beam
+w_0 = 1.3*5.275e-6;                                                       % [m] Initial waist plane 1/e^2 radius of the gaussian beam
 
-updates = 10;          % Number of times to update plot. If set higher than nz (such as Inf), script will simply update every step.
+updates = 100;          % Number of times to update plot. If set higher than nz (such as Inf), script will simply update every step.
 
 absorbertype = 4; % 1: No absorber, 2: constant absorber, 3: linear absorber, 4: quadratic absorber
 targetLx = 2*Lx_main; % [m] Full area x side length
 targetLy = 2*Ly_main;    % [m] Full area y side length
 alpha = 10e1; % [1/m] "Absorption coefficient", used for absorbertype 2
 beta = 10e4; % [1/m^2] "Absorption coefficient" per unit length distance out from edge of main area, used for absorbertype 3
-gamma = 3e8; % [1/m^3] "Absorption coefficient" per unit length distance out from edge of main area, squared, used for absorbertype 4
+gamma = 3e16; % [1/m^3] "Absorption coefficient" per unit length distance out from edge of main area, squared, used for absorbertype 4
 
-n_cladding = 1.01;%1.45;
-n_core = 1.01;%1.45;%1.46;
-core_radius = 5e-6;
+n_cladding = 1.45;
+n_core = 1.455;
+core_radius = 15e-6;
+% expectedRayleighrange = pi*w_0^2/(lambda/n_core)
+% expectedspotsize = w_0*sqrt(1+(Lz/expectedRayleighrange)^2)
 
 %% Initialization of space and frequency grids
 nz = round(Lz/targetzstepsize);                                       % Number of z steps
@@ -65,35 +69,51 @@ switch absorbertype
         absorber = exp(-dz*max(0,max(abs(Y) - Ly_main/2,abs(X) - Lx_main/2)).^2*gamma);
 end
 absorb_column = absorber(:);
-figure(1);
-% plot(x,absorber(:,round((ny-1)/2+1)));
+figure(1);clf;
 imagesc(x,y,absorber.');
-xlim([min(x) max(x)]);
+axis xy
+axis equal
+xlim([-Lx/2 Lx/2]);
+ylim([-Ly/2 Ly/2]);
+xlabel('x [m]');
+ylabel('y [m]');
+colorbar;
+title('Absorber illustration');
 
 k_0 = 2*pi/lambda;                                              % [m^-1] Wavenumber
 
 %% Initialisation of optical fibre parameters
-n_0 = 1;%n_cladding;
-n_mat = ones(nx,ny)*n_core;
-n_mat(sqrt(X.^2+Y.^2) > core_radius) = n_cladding;
-figure(1);clf;
+n_0 = n_core;
+% n_mat = ones(nx,ny)*n_cladding;
+% n_mat(sqrt((X-Lx_main/4).^2+Y.^2) < core_radius) = n_core;
+% n_mat(sqrt((X+Lx_main/4).^2+Y.^2) < core_radius) = n_core;
+% n_mat(abs(X-Lx_main/4) < core_radius & abs(Y) < core_radius) = n_core;
+% n_mat(abs(X+Lx_main/4) < core_radius & abs(Y) < core_radius) = n_core;
+% n_mat(sqrt(X.^2) > core_radius) = n_cladding;
+n_mat = max(n_cladding,n_core-(n_core-n_cladding)/core_radius^2*(X.^2+Y.^2));
+figure(2);clf;
 imagesc(x,y,n_mat.');
 axis xy
 axis equal
+xlim([-Lx/2 Lx/2]);
+ylim([-Ly/2 Ly/2]);
+colorbar;
+xlabel('x [m]');
+ylabel('y [m]');
+title('Refractive index');
 
 %% Beam initialization
-amplitude = exp(-(X.^2+Y.^2)/w^2);                             % Gaussian field amplitude
+% amplitude = exp(-((X-Lx_main/4).^2+Y.^2)/w_0^2) - exp(-((X+Lx_main/4).^2+Y.^2)/w_0^2);                             % Gaussian field amplitude
+amplitude = exp(-((X-Lx_main/10).^2+Y.^2)/w_0^2);                             % Gaussian field amplitude
 phase = 0*X;                                         % Phase
-E_ref = amplitude.*exp(1i*phase);                      % Electric field
+E = amplitude.*exp(1i*phase);                      % Electric field
+E = E/sqrt(sum(abs(E(:)).^2));
 
 %% Fibre propagation and plotting
 %% Sequential method initialization
 [nvec,~,invec] = uniquetol(n_mat);
-% ax = dz/(4i*dx^2*k_0*n);
-% ay = dz/(4i*dy^2*k_0*n);
-% [e,~,ie] = uniquetol(dz*k_0*(n.^2-n_0^2)/n);
-% b  = 1 + 2*ax + 2*ay + dz*1i*k_0*delta_n_2_column/n_0;
-% c  = 1 - 2*ax - 2*ay - dz*1i*k_0*delta_n_2_column/n_0;
+ax = dz/(4i*dx^2*k_0*n_0);
+ay = dz/(4i*dy^2*k_0*n_0);
 
 %% Vectorized method initialization
 % delta_n_2 = n.^2-n_0^2;                              %delta_n^2 in the expression for FD BPM
@@ -121,145 +141,153 @@ E_ref = amplitude.*exp(1i*phase);                      % Electric field
 % M_rhs = M_iMinus1+M_iPlus1+M_i_rhs+M_jMinus1+M_jPlus1;
 
 %% Figure initialization
-figure(2);
-h_line = semilogy(x,abs(E_ref(:,(ny-1)/2+1).').^2); 
+figure(3);clf;
+h_line = semilogy(x,abs(E(:,round((ny-1)/2+1)).').^2); 
 h_title = title(['x intensity profile at z = 0 m (second moment radius = ' num2str(2*std(x,h_line.YData)) ' m)']);
 line([-Lx_main/2 -Lx_main/2 NaN Lx_main/2 Lx_main/2],[ylim NaN ylim],'Linestyle','--','Color','k');
 axis([min(x) max(x) 1e-9 1]);
+xlabel('x [m]');
+ylabel('Intensity [W/m^2]');
 
-updatesliceindices = unique(round(linspace(1,nz,min(nz,updates))));
-nextupdatesliceindicesindex = 1;
+updatesliceindices = unique(round(linspace(0,nz,min(nz,updates))));
+z_updates = dz*updatesliceindices;
+nextupdatesliceindicesindex = 2;
 
-figure(3);
+figure(4);clf;
 subplot(2,1,1);
-h_im1 = imagesc(x,y,abs(E_ref.').^2);
-axis xy
-axis equal
-% xlim([-Lx_main/2 Lx_main/2]);
-% ylim([-Ly_main/2 Ly_main/2]);
+hold on;
+h_im1 = imagesc(x,y,abs(E.').^2);
+axis xy;
+axis equal;
 xlim([-Lx/2 Lx/2]);
 ylim([-Ly/2 Ly/2]);
+colorbar;
+caxis('manual');
+xlabel('x [m]');
+ylabel('y [m]');
+title('Intensity [W/m^2]');
+contour(X,Y,n_mat,(n_cladding+eps(n_cladding))*[1 1],'color','r','linestyle','--');
 subplot(2,1,2);
-h_im2 = imagesc(x,y,angle(E_ref.'));
-axis xy
-axis equal
-% xlim([-Lx_main/2 Lx_main/2]);
-% ylim([-Ly_main/2 Ly_main/2]);
+hold on;
+h_im2 = imagesc(x,y,angle(E.'));
+axis xy;
+axis equal;
 xlim([-Lx/2 Lx/2]);
 ylim([-Ly/2 Ly/2]);
+colorbar;
+caxis([-pi pi]);
+contour(X,Y,n_mat,(n_cladding+eps(n_cladding))*[1 1],'color','r','linestyle','--');
+xlabel('x [m]');
+ylabel('y [m]');
+title('Phase [rad]');
 
-%For plotting the diverged gaussian field from gaussian_Prop_2D.m
-% NxNy_ref = size(E_ref);
-% Nx_ref=NxNy_ref(1);
-% Nx_ref_start=round(Nx_ref/2)-round(nx/2);
-% Nx_ref_end=round(Nx_ref/2)+round(nx/2)-1;
+powers = NaN(1,min(nz,updates));
+powers(1) = sum(abs(E(:)).^2);
+figure(5);clf;
+plot(z_updates,powers,'YDataSource','powers','linewidth',2);
+xlim([0 Lz]);
+xlabel('Propagation distance [m]');
+ylabel('Relative power remaining');
 
 %% Sequential method execution
-E = E_ref;
+multiplier = exp(-1i*dz*k_0*(n_mat.^2 - n_0^2)/n_0).*absorber;
 d = zeros(nx,ny);
+b = zeros(max(nx,ny),1);
 for zidx = 1:nz
     %% Explicit part of step 1
     for iy = 1:ny
         for ix = 1:nx % construct d, the right hand side of the system of linear equations
-            i = sub2ind([nx,ny],ix,iy);
-			n = nvec(invec(i));
             delta = 0;
-            if ix ~= 1;  delta = delta +   dz/(4i*dx^2*k_0*n)*(E(ix-1,iy)-E(ix,iy)); end
-            if ix ~= nx; delta = delta +   dz/(4i*dx^2*k_0*n)*(E(ix+1,iy)-E(ix,iy)); end
-%             if iy ~= 1;  delta = delta + 2*dz/(4i*dy^2*k_0*n)*(E(ix,iy-1)-E(ix,iy)); end
-%             if iy ~= ny; delta = delta + 2*dz/(4i*dy^2*k_0*n)*(E(ix,iy+1)-E(ix,iy)); end
-            d(ix,iy) = E(ix,iy)*(1 - 1i*dz*k_0*(n^2 - n_0^2)/n) + delta;
+            if ix ~= 1;  delta = delta +   ax*(E(ix-1,iy)-E(ix,iy)); end
+            if ix ~= nx; delta = delta +   ax*(E(ix+1,iy)-E(ix,iy)); end
+            if iy ~= 1;  delta = delta + 2*ay*(E(ix,iy-1)-E(ix,iy)); end
+            if iy ~= ny; delta = delta + 2*ay*(E(ix,iy+1)-E(ix,iy)); end
+            d(ix,iy) = E(ix,iy) + delta;
         end
     end
 
     %% Implicit part of step 1, Thomas algorithm along x, sweeps up from 2 to nx and then down from nx to 1
     for iy = 1:ny
         % Forward sweep, index 1 and 2:
-		i = sub2ind([nx,ny],1,iy);
-		n = nvec(invec(i));
-        b(1) = 1 + dz/(4i*dx^2*k_0*n) + 1i*dz*k_0*(n^2 - n_0^2)/n;
-		i = sub2ind([nx,ny],2,iy);
-		n = nvec(invec(i));
-        w = -dz/(4i*dx^2*k_0*n)/b(1);
-        b(2) = 1 + 2*dz/(4i*dx^2*k_0*n) + 1i*dz*k_0*(n^2 - n_0^2)/n + w*dz/(4i*dx^2*k_0*     n     );
+        b(1) = 1 + ax;
+        w = -ax/b(1);
+        b(2) = 1 + 2*ax + w*ax;
         d(2,iy) = d(2,iy) - w*d(1,iy);
         % Forward sweep, indices 3 to nx-1:
         for ix = 3:nx-1
-			i = sub2ind([nx,ny],ix,iy);
-			n = nvec(invec(i));
-            w = -dz/(4i*dx^2*k_0*n)/b(ix-1);
-            b(ix) = 1 + 2*dz/(4i*dx^2*k_0*n) + 1i*dz*k_0*(n^2 - n_0^2)/n;
-            b(ix) = b(ix) + w*dz/(4i*dx^2*k_0*     n     );
+            w = -ax/b(ix-1);
+            b(ix) = 1 + 2*ax;
+            b(ix) = b(ix) + w*ax;
             d(ix,iy) = d(ix,iy) - w*d(ix-1,iy);
         end
         % Forward sweep, index nx:
-		i = sub2ind([nx,ny],nx,iy);
-		n = nvec(invec(i));
-        w = -dz/(4i*dx^2*k_0*n)/b(nx-1);
-        b(nx) = 1 + dz/(4i*dx^2*k_0*n) + 1i*dz*k_0*(n^2 - n_0^2)/n;
-        b(nx) = b(nx) + w*dz/(4i*dx^2*k_0*     n     );
+        w = -ax/b(nx-1);
+        b(nx) = 1 + ax;
+        b(nx) = b(nx) + w*ax;
         d(nx,iy) = d(nx,iy) - w*d(nx-1,iy);
 
         % Back sweep, index nx:
         d(nx,iy) = d(nx,iy)/b(nx);
         % Back sweep, indices nx-1 to 2:
         for ix = nx-1:-1:2
-			i = sub2ind([nx,ny],ix,iy);
-			n = nvec(invec(i));
-            d(ix,iy) = (d(ix,iy) + dz/(4i*dx^2*k_0*n)*d(ix+1,iy))/b(ix);
+            d(ix,iy) = (d(ix,iy) + ax*d(ix+1,iy))/b(ix);
         end
         % Back sweep, index 1:
-		i = sub2ind([nx,ny],1,iy);
-		n = nvec(invec(i));
-        d(1,iy) = (d(1,iy) + dz/(4i*dx^2*k_0*n)*d(2,iy))/b(1);
+        d(1,iy) = (d(1,iy) + ax*d(2,iy))/b(1);
     end
-    E = d;
-%     %% Explicit part of step 2
-%     for iy = 1:ny
-%         for ix = 1:nx % construct d, the right hand side of the system of linear equations
-%             i = sub2ind([nx,ny],ix,iy);
-%             delta = 0;
-%             if iy ~= 1;  delta = delta - ay*(E(ix,iy-1)-E(ix,iy)); end
-%             if iy ~= ny; delta = delta - ay*(E(ix,iy+1)-E(ix,iy)); end
-%             d(ix,iy) = d(ix,iy) + delta;
-%         end
-%     end
-%     
-%     %% Implicit part of step 2, Thomas algorithm along y, sweeps up from 2 to ny and then down from ny to 1
-%     for ix = 1:nx
-%         % Forward sweep, index 2:
-%         b(1) = 1 + ay;
-%         w = -ay/b(1);
-%         b(2) = 1 + 2*ay + w*ay;
-%         d(ix,2) = d(ix,2) - w*d(ix,1);
-%         % Forward sweep, indices 3 to ny-1:
-%         for iy = 3:ny-1
-%             w = -ay/b(iy-1);
-%             b(iy) = 1 + 2*ay;
-%             b(iy) = b(iy) + w*ay;
-%             d(ix,iy) = d(ix,iy) - w*d(ix,iy-1);
-%         end
-%         % Forward sweep, index ny:
-%         w = -ay/b(ny-1);
-%         b(ny) = 1 + ay;
-%         b(ny) = b(ny) + w*ay;
-%         d(ix,ny) = d(ix,ny) - w*d(ix,ny-1);
-% 
-%         % Back sweep, index ny:
-%         E(ix,ny) = d(ix,ny)/b(ny);
-%         % Back sweep, indices ny-1 to 2:
-%         for iy = ny-1:-1:2
-%             E(ix,iy) = (d(ix,iy) + ay*E(ix,iy+1))/b(iy);
-%         end
-%         % Back sweep, index 1:
-%         E(ix,1) = (d(ix,1) + ay*E(ix,2))/b(1);
-%     end
+    
+    %% Explicit part of step 2
+    for iy = 1:ny
+        for ix = 1:nx % construct d, the right hand side of the system of linear equations
+            i = sub2ind([nx,ny],ix,iy);
+            delta = 0;
+            if iy ~= 1;  delta = delta - ay*(E(ix,iy-1)-E(ix,iy)); end
+            if iy ~= ny; delta = delta - ay*(E(ix,iy+1)-E(ix,iy)); end
+            d(ix,iy) = d(ix,iy) + delta;
+        end
+    end
+    
+    %% Implicit part of step 2, Thomas algorithm along y, sweeps up from 2 to ny and then down from ny to 1
+    for ix = 1:nx
+        % Forward sweep, index 2:
+        b(1) = 1 + ay;
+        w = -ay/b(1);
+        b(2) = 1 + 2*ay + w*ay;
+        d(ix,2) = d(ix,2) - w*d(ix,1);
+        % Forward sweep, indices 3 to ny-1:
+        for iy = 3:ny-1
+            w = -ay/b(iy-1);
+            b(iy) = 1 + 2*ay;
+            b(iy) = b(iy) + w*ay;
+            d(ix,iy) = d(ix,iy) - w*d(ix,iy-1);
+        end
+        % Forward sweep, index ny:
+        w = -ay/b(ny-1);
+        b(ny) = 1 + ay;
+        b(ny) = b(ny) + w*ay;
+        d(ix,ny) = d(ix,ny) - w*d(ix,ny-1);
 
-    h_line.YData = abs(E(:,round((ny-1)/2+1))).^2;
-    h_title.String = ['x intensity profile at z = ' num2str(zidx*dz,'%.1e') ' m (second moment radius = ' num2str(2*std(x,h_line.YData),'%.6e') ' m)'];
-    h_im1.CData = abs(E.').^2;
-    h_im2.CData = angle(E.');
-    drawnow;
+        % Back sweep, index ny:
+        E(ix,ny) = d(ix,ny)/b(ny);
+        % Back sweep, indices ny-1 to 2:
+        for iy = ny-1:-1:2
+            E(ix,iy) = (d(ix,iy) + ay*E(ix,iy+1))/b(iy);
+        end
+        % Back sweep, index 1:
+        E(ix,1) = (d(ix,1) + ay*E(ix,2))/b(1);
+    end
+    E = E.*multiplier;
+
+    if zidx == updatesliceindices(nextupdatesliceindicesindex)
+        powers(nextupdatesliceindicesindex) = sum(abs(E(:)).^2);
+        h_line.YData = abs(E(:,round((ny-1)/2+1))).^2;
+        h_title.String = ['x intensity profile at z = ' num2str(zidx*dz,'%.1e') ' m (second moment radius = ' num2str(2*std(x,h_line.YData),'%.6e') ' m)'];
+        h_im1.CData = abs(E.').^2;
+        h_im2.CData = angle(E.');
+        refreshdata(5);
+        drawnow;
+        nextupdatesliceindicesindex = nextupdatesliceindicesindex + 1;
+    end
 end
 
 
