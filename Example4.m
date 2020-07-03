0c7a42ef5c45c8e8f3af4be90d15dd6f81570695
Madhu Veettikazhy <madve@fotonik.dtu.dk>
2020-07-03 23:05:57 +0200

    WIP: Added examples 4 (2D GRIN lens) and 5 (1D GRIN lens)

---
+++ Example4.m
@@ -0,0 +1,86 @@
+clear P % Parameters struct
+
+% This example shows a basic case of launching a Gaussian beam
+% into the GRINTECH lens GT-CFRL-100-025-20-CC (810) realized using FDBPM 
+% and the subsequent propagation of the E field from GRIN lens's output in
+% air using FFTBPM
+
+%% Part 1 run with FDBPM
+%% General and solver-related settings
+P.name = mfilename;
+P.useAllCPUs = true;
+P.useGPU = false;
+P.intNorm = false; % Choose true for field to be normalized w.r.t. max intensity, false to normalize such that total power is 1
+
+%% Visualization parameters
+P.figNum = 1;
+P.saveVideo = false; % To save the field intensity and phase profiles at different transverse planes
+P.updates = 100;            % Number of times to update plot. Must be at least 1, showing the final state.
+P.downsampleImages = false; % Due to a weird MATLAB bug, MATLAB may crash when having created imagesc (or image) plots with dimensions larger than roughly 2500x2500 and then calling mex functions repeatedly. This flag will enable downsampling to 500x500 of all data before plotting, hopefully avoiding the issue.
+P.displayScaling = 1;  % Zooms in on figures 1 & 3a,b. Set to 1 for no zooming.  
+% P.plotEmax = 0.5; % Max of color scale in the intensity plot 
+
+%% Resolution-related parameters (check for convergence)
+P.Lx_main = 0.7e-3;        % [m] x side length of main area
+P.Ly_main = 0.7e-3;        % [m] y side length of main area
+P.Nx_main = 800;          % x resolution of main area
+P.Ny_main = 800;          % y resolution of main area
+P.padfactor = 1.5;  % How much absorbing padding to add on the sides of the main area (1 means no padding, 2 means the absorbing padding on both sides is of thickness Lx_main/2)
+P.dz_target = 2e-6; % [m] z step size to aim for
+P.alpha = 3e14;             % [1/m^3] "Absorption coefficient" per squared unit length distance out from edge of main area
+
+%% Problem definition
+P.lambda = 810e-9; % [m] Wavelength
+P.n_cladding = 1.0; % [] Cladding refractive index
+P.n_0 = 1.521;
+P.Lz = 6.05e-3; % [m] z propagation distances for this segment
+
+% In the shapes 2D array, each row is a shape such as a core in a fiber.
+% Column 1 are the x coordinates, column 2 are the y coordinates, column 3
+% are radii, column 4 are the types of the shapes, column 5 are the peak
+% refractive indices and column 6 is the g parameter, only needed if any of
+% the shapes are GRIN lenses.
+
+% Shape types are 1: Circular step-index disk, 2: Antialiased circular
+% step-index disk, 3: Parabolic graded index disk, 4: GRIN lens focusing in
+% both x and y, 5: GRIN lens focusing only in y.
+P.shapes = [ 0 0 0.5e-3  4  1.521 260];
+
+% P.E can be either a function that takes X, Y and Eparameters as inputs
+% and provides the complex E field as output, or it can be a (2x1) cell array in
+% which the first cell is a complex E field matrix and the second cell is a
+% (2x1) array [Lx, Ly] that describe the side lengths of the provided E
+% matrix. In the case of a cell array, the provided E field will be adapted
+% to the new grid using the interp2 function.
+P.E = @calcInitialE; % Defined at the end of this file
+
+%% Run solver
+[E_out,shapes_out] = FD_BPM(P);
+
+%% Output E from FDBPM propagating in air with FFTBPM
+P.name = 'Example4_FFT';
+P.figNum = 2;
+P.saveVideo = false; % To save the field intensity and phase profiles at different transverse planes
+P.updates = 30;            % Number of times to update plot. Must be at least 1, showing the final state.
+
+P.Lx_main = 500e-6;        % [m] x side length of main area
+P.Ly_main = 500e-6;        % [m] y side length of main area
+P.Nx_main = 1000;          % x resolution of main area
+P.Ny_main = 1000;          % y resolution of main area
+P.dz_target = 1e-4; % [m] z step size to aim for
+P.alpha = 8e13;             % [1/m^3] "Absorption coefficient" per squared unit length distance out from edge of main area
+P.Lz = 1e-2; % [m] z propagation distances for this segment
+
+P.E = E_out;
+
+%% Run solver
+[E_out] = FFT_BPM(P);
+
+%% USER DEFINED E-FIELD INITIALIZATION FUNCTION
+function E = calcInitialE(X,Y,Eparameters) % Function to determine the initial E field. Eparameters is a cell array of additional parameters such as beam size
+w_0 = 5e-6;
+offset = 0;
+amplitude = exp(-((X-offset).^2+Y.^2)/w_0^2);
+phase = zeros(size(X));
+E = amplitude.*exp(1i*phase); % Electric field
+end
