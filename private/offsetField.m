ecc095050db50cb4f49914f6836b0cadcf13126c
Anders Kragh Hansen <ankrh@fotonik.dtu.dk>
2022-03-30 13:55:14 +0200

    Added error messages explaining that offsetField and tiltField does not currently work when the segment input field is a function handle. A more elegant solution may be implemented in the future.

--- private/offsetField.m
+++ private/offsetField.m
@@ -1,4 +1,7 @@
 function P = offsetField(P,direction,distance)
+if isa(P.E,'function_handle')
+  error('Error: BPM-Matlab currently doesn''t support field offsets in segments where the field is specified as a function handle. You can avoid this by adding a short initial propagation segment and applying the field offset between the first and second segments.');
+end
 [X,Y] = ndgrid(P.x,P.y);
 P.E.field = interpn(X + distance*cosd(direction),Y + distance*sind(direction),P.E.field,X,Y,'linear',0);
 end
--- private/tiltField.m
+++ private/tiltField.m
@@ -1,4 +1,7 @@
 function P = tiltField(P,direction,angle)
+if isa(P.E,'function_handle')
+  error('Error: BPM-Matlab currently doesn''t support field tilts in segments where the field is specified as a function handle. You can avoid this by adding a short initial propagation segment and applying the field tilt between the first and second segments.');
+end
 [X,Y] = ndgrid(P.x,P.y);
 k = P.n_0*2*pi/P.lambda*angle/180*pi; % Refractive index in wedge in between fiber segments assumed to be uniformly P.n_0
 P.E.field = P.E.field.*exp(-1i*k*(cosd(direction).*X + sind(direction).*Y));
